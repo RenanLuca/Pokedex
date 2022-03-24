569f68b9a20d0969c0efcb12a79fb6717556f1ba
RenanLuca <renandelucamachado3232@gmail.com>
2022-03-24 17:33:25 -0300

    api fix

--- script/script.js
+++ script/script.js
@@ -1,14 +1,21 @@
-const fetchPokemon = () => {
-    const url = 'https://pokeapi.co/api/v2/pokemon/25/'
-   
-    fetch(url)
-   .then(response => response.json())
-    .then( pokemon => {
-        console.log(pokemon)
-    })
-    
-    
+const fetchPokemon = {
+    indice: 1,    
+    getPokemonUrl: id => `https://pokeapi.co/api/v2/pokemon/${id}`,
+    getPokemon: () => {
+        fetch( fetchPokemon.getPokemonUrl(fetchPokemon.indice))
+        .then( response => response.json())
+        .then( pokemon => console.log(pokemon))
+    }
 }
 
 
-fetchPokemon()
+fetchPokemon.getPokemon()
+
+
+
+
+
+
+
+
+
